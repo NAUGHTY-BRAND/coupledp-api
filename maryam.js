e4ba2e8383d2ecd506358c52febdb15a080129e4
ÑAUGHTY BRAND <145090660+NAUGHTY-BRAND@users.noreply.github.com>
2024-07-31 19:56:55 +0500

    Update and rename index.js to maryam.js

--- maryam.js
+++ maryam.js
@@ -1,21 +1,18 @@
 const express = require('express');
 const fs = require('fs');
-const _ = require('lodash'); // Import Lodash library for array shuffling
-const data = require('./data.json'); // Assuming the data is stored in data.json file
+const maryam = require('lodash');
+const data = require('./dps.json');
 
 const app = express();
-let shuffledData = _.shuffle(data); // Shuffle the data array initially
-const sentUrls = []; // List to keep track of previously sent image URLs
+let shuffledData = maryam.shuffle(data);
+const sentUrls = [];
 
-app.use(express.json()); // Middleware to parse incoming JSON request bodies
-
-// Endpoint to get a random matching male and female image URLs
+app.use(express.json());
 app.get('/coupledp', async (req, res) => {
   let matchingUrls;
   while (!matchingUrls || sentUrls.includes(matchingUrls.maleUrl) || sentUrls.includes(matchingUrls.femaleUrl)) {
-    // Shuffle the data array again if all images have been sent
     if (sentUrls.length === shuffledData.length) {
-      shuffledData = _.shuffle(data);
+      shuffledData = maryam.shuffle(data);
       sentUrls.length = 0;
     }
     const randomIndex = Math.floor(Math.random() * shuffledData.length);
@@ -24,11 +21,9 @@ app.get('/coupledp', async (req, res) => {
   
   sentUrls.push(matchingUrls.maleUrl, matchingUrls.femaleUrl);
   
-  console.log('GET request received for matching-images');
+  console.log('Req Recived For Couple DP');
   res.json(matchingUrls);
 });
-
-// Endpoint to add a new matching male and female image URLs to the data
 app.get('/addcoupledp', async (req, res) => {
   const maleUrl = req.query.maleUrl;
   const femaleUrl = req.query.femaleUrl;
@@ -38,7 +33,7 @@ app.get('/addcoupledp', async (req, res) => {
   }
 
   data.push({ maleUrl, femaleUrl });
-  shuffledData = _.shuffle(data); // Shuffle the data array whenever new images are added
+  shuffledData = maryam.shuffle(data);
 
   fs.writeFile('data.json', JSON.stringify(data, null, 2), (err) => {
     if (err) {
@@ -50,8 +45,6 @@ app.get('/addcoupledp', async (req, res) => {
     res.send('Matching images added successfully');
   });
 });
-
-// Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Something went wrong!');
